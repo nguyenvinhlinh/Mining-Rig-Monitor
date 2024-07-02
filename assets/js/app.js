481d55b23a773848bc736b2e4b62d9647fac79fb
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2024-07-02 21:12:15 +0700

    Implement dark mode in flowbite.

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,6 +24,7 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+import "../vendor/dark-mode.js"
 
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
