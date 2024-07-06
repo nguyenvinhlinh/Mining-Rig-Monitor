2362be63f609674aa60ecbc88f2051dc1c597fbe
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2024-07-06 19:28:05 +0700

    Add flowbite_component , flowbite_table function.

--- lib/mining_rig_monitor_web/components/flowbite_components.ex
+++ lib/mining_rig_monitor_web/components/flowbite_components.ex
@@ -52,131 +52,40 @@ defmodule MiningRigMonitorWeb.FlowbiteComponents do
         assign(assigns, row_id: assigns.row_id || fn {id, _item} -> id end)
       end
 
-    a = ~H"""
-    <div class="overflow-hidden shadow">
-        <table class="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-600">
-          <thead class="bg-gray-100 dark:bg-gray-700">
-            <tr>
-              <th scope="col" class="p-4">
-                <div class="flex items-center">
-                  <input id="checkbox-all" aria-describedby="checkbox-1" type="checkbox" class="w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600">
-                  <label for="checkbox-all" class="sr-only">checkbox</label>
-                </div>
-              </th>
-              <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                Mining Rig Name
-              </th>
-              <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                Mining Coin
-              </th>
-              <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                Max CPU Temp.
-              </th>
-              <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                Max GPU Temp.
-              </th>
-              <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                Max GPU Fan
-              </th>
-              <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                Power (Walt)
-              </th>
-              <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                Uptime
-              </th>
-              <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                Actions
-              </th>
-            </tr>
-          </thead>
-          <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
-            <tr class="hover:bg-gray-100 dark:hover:bg-gray-700">
-              <td class="w-4 p-4">
-                <div class="flex items-center">
-                  <input id="checkbox-20" aria-describedby="checkbox-1" type="checkbox" class="w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600">
-                  <label for="checkbox-20" class="sr-only">checkbox</label>
-                </div>
-              </td>
-
-              <td class="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">Thanh Long</td>
-              <td class="max-w-sm p-4 overflow-hidden text-base font-normal text-gray-500 truncate xl:max-w-xs dark:text-gray-400">
-                Monero
-              </td>
-              <td class="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">60C</td>
-              <td class="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">80C</td>
-              <td class="p-4 text-base font-normal text-gray-900 whitespace-nowrap dark:text-white">
-                100%
-              </td>
-              <td class="p-4 text-base font-normal text-gray-900 whitespace-nowrap dark:text-white">
-                250W
-              </td>
-              <td class="p-4 text-base font-normal text-gray-900 whitespace-nowrap dark:text-white">
-                1 days 16 hours 30 minutes
-              </td>
-
-              <td class="p-4 space-x-2 whitespace-nowrap">
-                <button type="button" data-modal-toggle="edit-user-modal" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center
-                              text-white rounded-lg bg-blue-700 hover:bg-blue-800
-                              focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                  <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path><path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path></svg>
-                  Edit
-                </button>
-                <button type="button" data-modal-toggle="delete-user-modal" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900">
-                  <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg>
-                  Delete
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    """
-
     ~H"""
-    <div class="overflow-y-auto px-4 sm:overflow-visible sm:px-0">
-      <table class="w-[40rem] mt-11 sm:w-full">
-        <thead class="text-sm text-left leading-6 text-zinc-500">
+    <div class="overflow-hidden shadow">
+      <table class="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-600">
+        <thead class="bg-gray-100 dark:bg-gray-700">
           <tr>
-            <th :for={col <- @col} class="p-0 pb-4 pr-6 font-normal"><%= col[:label] %></th>
-            <th :if={@action != []} class="relative p-0 pb-4">
-              <span class="sr-only"><%= gettext("Actions") %></span>
+            <th :for={col <- @col} class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
+              <%= col[:label] %>
+            </th>
+            <th :if={@action != []} class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
+              <%= gettext("Actions") %>
             </th>
           </tr>
         </thead>
-        <tbody
-          id={@id}
-          phx-update={match?(%Phoenix.LiveView.LiveStream{}, @rows) && "stream"}
-          class="relative divide-y divide-zinc-100 border-t border-zinc-200 text-sm leading-6 text-zinc-700"
-        >
-          <tr :for={row <- @rows} id={@row_id && @row_id.(row)} class="group hover:bg-zinc-50">
+          <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700"
+                 id={@id}
+                 phx-update={match?(%Phoenix.LiveView.LiveStream{}, @rows) && "stream"} >
+            <tr :for={row <- @rows} id={@row_id && @row_id.(row)} class="hover:bg-gray-100 dark:hover:bg-gray-700">
             <td
               :for={{col, i} <- Enum.with_index(@col)}
               phx-click={@row_click && @row_click.(row)}
-              class={["relative p-0", @row_click && "hover:cursor-pointer"]}
-            >
-              <div class="block py-4 pr-6">
-                <span class="absolute -inset-y-px right-0 -left-4 group-hover:bg-zinc-50 sm:rounded-l-xl" />
-                <span class={["relative", i == 0 && "font-semibold text-zinc-900"]}>
-                  <%= render_slot(col, @row_item.(row)) %>
-                </span>
-              </div>
+              class={["p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white", @row_click && "hover:cursor-pointer"]}
+              >
+                <%= render_slot(col, @row_item.(row)) %>
+
             </td>
-            <td :if={@action != []} class="relative w-14 p-0">
-              <div class="relative whitespace-nowrap py-4 text-right text-sm font-medium">
-                <span class="absolute -inset-y-px -right-4 left-0 group-hover:bg-zinc-50 sm:rounded-r-xl" />
-                <span
-                  :for={action <- @action}
-                  class="relative ml-4 font-semibold leading-6 text-zinc-900 hover:text-zinc-700"
-                >
-                  <%= render_slot(action, @row_item.(row)) %>
-                </span>
-              </div>
+            <td :if={@action != []} class="p-4 space-x-2 whitespace-nowrap">
+              <%= for action <- @action do %>
+                <%= render_slot(action, @row_item.(row)) %>
+              <% end %>
             </td>
           </tr>
         </tbody>
       </table>
     </div>
     """
-
   end
 end
