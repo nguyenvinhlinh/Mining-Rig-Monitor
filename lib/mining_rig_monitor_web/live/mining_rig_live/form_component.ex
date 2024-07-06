35ed227c55653e06dad45e99053966f8f98b8557
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2024-07-06 17:49:08 +0700

    add new mininig rig feature.

--- lib/mining_rig_monitor_web/live/mining_rig_live/form_component.ex
+++ lib/mining_rig_monitor_web/live/mining_rig_live/form_component.ex
@@ -9,7 +9,7 @@ defmodule MiningRigMonitorWeb.MiningRigLive.FormComponent do
     <div>
       <.header>
         <%= @title %>
-        <:subtitle>Use this form to manage mining_rig records in your database.</:subtitle>
+        <:subtitle>Use this form to manage mining rig records in your database.</:subtitle>
       </.header>
 
       <.simple_form
@@ -20,45 +20,6 @@ defmodule MiningRigMonitorWeb.MiningRigLive.FormComponent do
         phx-submit="save"
       >
         <.input field={@form[:name]} type="text" label="Name" />
-        <.input field={@form[:code]} type="text" label="Code" />
-        <.input field={@form[:cpu_1_name]} type="text" label="Cpu 1 name" />
-        <.input field={@form[:cpu_2_name]} type="text" label="Cpu 2 name" />
-        <.input field={@form[:ram_1_size]} type="text" label="Ram 1 size" />
-        <.input field={@form[:ram_2_size]} type="text" label="Ram 2 size" />
-        <.input field={@form[:ram_3_size]} type="text" label="Ram 3 size" />
-        <.input field={@form[:ram_4_size]} type="text" label="Ram 4 size" />
-        <.input field={@form[:ram_1_type]} type="text" label="Ram 1 type" />
-        <.input field={@form[:ram_2_type]} type="text" label="Ram 2 type" />
-        <.input field={@form[:ram_3_type]} type="text" label="Ram 3 type" />
-        <.input field={@form[:ram_4_type]} type="text" label="Ram 4 type" />
-        <.input field={@form[:ram_1_manufacture]} type="text" label="Ram 1 manufacture" />
-        <.input field={@form[:ram_2_manufacture]} type="text" label="Ram 2 manufacture" />
-        <.input field={@form[:ram_3_manufacture]} type="text" label="Ram 3 manufacture" />
-        <.input field={@form[:ram_4_manufacture]} type="text" label="Ram 4 manufacture" />
-        <.input field={@form[:ram_1_part_number]} type="text" label="Ram 1 part number" />
-        <.input field={@form[:ram_2_part_number]} type="text" label="Ram 2 part number" />
-        <.input field={@form[:ram_3_part_number]} type="text" label="Ram 3 part number" />
-        <.input field={@form[:ram_4_part_number]} type="text" label="Ram 4 part number" />
-        <.input field={@form[:vga_1_name]} type="text" label="Vga 1 name" />
-        <.input field={@form[:vga_2_name]} type="text" label="Vga 2 name" />
-        <.input field={@form[:vga_3_name]} type="text" label="Vga 3 name" />
-        <.input field={@form[:vga_4_name]} type="text" label="Vga 4 name" />
-        <.input field={@form[:vga_1_driver_version]} type="text" label="Vga 1 driver version" />
-        <.input field={@form[:vga_2_driver_version]} type="text" label="Vga 2 driver version" />
-        <.input field={@form[:vga_3_driver_version]} type="text" label="Vga 3 driver version" />
-        <.input field={@form[:vga_4_driver_version]} type="text" label="Vga 4 driver version" />
-        <.input field={@form[:vga_1_vbios_version]} type="text" label="Vga 1 vbios version" />
-        <.input field={@form[:vga_2_vbios_version]} type="text" label="Vga 2 vbios version" />
-        <.input field={@form[:vga_3_vbios_version]} type="text" label="Vga 3 vbios version" />
-        <.input field={@form[:vga_4_vbios_version]} type="text" label="Vga 4 vbios version" />
-        <.input field={@form[:vga_1_total_memory]} type="text" label="Vga 1 total memory" />
-        <.input field={@form[:vga_2_total_memory]} type="text" label="Vga 2 total memory" />
-        <.input field={@form[:vga_3_total_memory]} type="text" label="Vga 3 total memory" />
-        <.input field={@form[:vga_4_total_memory]} type="text" label="Vga 4 total memory" />
-        <.input field={@form[:vga_1_pci_bus_id]} type="text" label="Vga 1 pci bus" />
-        <.input field={@form[:vga_2_pci_bus_id]} type="text" label="Vga 2 pci bus" />
-        <.input field={@form[:vga_3_pci_bus_id]} type="text" label="Vga 3 pci bus" />
-        <.input field={@form[:vga_4_pci_bus_id]} type="text" label="Vga 4 pci bus" />
         <:actions>
           <.button phx-disable-with="Saving...">Save Mining rig</.button>
         </:actions>
@@ -103,7 +64,10 @@ defmodule MiningRigMonitorWeb.MiningRigLive.FormComponent do
   end
 
   defp save_mining_rig(socket, :new, mining_rig_params) do
-    case MiningRigs.create_mining_rig(mining_rig_params) do
+    code = UUID.uuid1()
+    mod_mining_rig_params = Map.put(mining_rig_params, "code", code)
+
+    case MiningRigs.create_mining_rig(mod_mining_rig_params) do
       {:ok, mining_rig} ->
         notify_parent({:saved, mining_rig})
 
