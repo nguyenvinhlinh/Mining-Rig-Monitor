cd5969123ca59b0a2211e23ece515a02bfde1086
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2024-07-17 15:17:43 +0700

    implement api to save asic_rig_monitor_record.

--- lib/mining_rig_monitor_web/controllers/asic_rig_monitor_record_controller.ex
+++ lib/mining_rig_monitor_web/controllers/asic_rig_monitor_record_controller.ex
@@ -1,10 +1,16 @@
 defmodule MiningRigMonitorWeb.AsicRigMonitorRecordController do
   use MiningRigMonitorWeb, :controller
 
+  alias MiningRigMonitor.AsicRigMonitorRecords
+
+  action_fallback MiningRigMonitorWeb.FallbackController
+
   def save(conn, params) do
-    IO.inspect "DEBUG #{__ENV__.file} @#{__ENV__.line}"
-    IO.inspect conn
-    IO.inspect "END"
-    json(conn, :ok)
+    new_params = params
+    |> Map.put("mining_rig_id", conn.assigns.mining_rig.id)
+    case AsicRigMonitorRecords.create_asic_rig_monitor_record(new_params) do
+      {:ok, _record} -> json(conn, :ok)
+      {:error, changeset} -> {:error, changeset}
+    end
   end
 end
