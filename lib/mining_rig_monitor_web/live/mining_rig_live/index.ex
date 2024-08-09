ac2c684982dce92121a4ff61e700a7733a247479
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2024-08-09 17:45:20 +0700

    fix bug append rig into wrong list after saved.

--- lib/mining_rig_monitor_web/live/mining_rig_live/index.ex
+++ lib/mining_rig_monitor_web/live/mining_rig_live/index.ex
@@ -57,14 +57,19 @@ defmodule MiningRigMonitorWeb.MiningRigLive.Index do
     |> assign(:mining_rig, MiningRigs.get_mining_rig!(id))
   end
 
-
-
   @impl true
   def handle_info({MiningRigMonitorWeb.MiningRigLive.FormComponent, {:saved, mining_rig}}, socket) do
-    new_socket = socket
-    |> stream_insert(:new_mining_rig_list, mining_rig)
-
-    {:noreply, new_socket}
+    case mining_rig.type do
+      nil ->
+        new_socket = stream_insert(socket, :new_mining_rig_list, mining_rig)
+        {:noreply, new_socket}
+      "cpu_gpu" ->
+        new_socket = stream_insert(socket, :cpu_gpu_mining_rig_list, mining_rig)
+        {:noreply, new_socket}
+      "asic" ->
+        new_socket = stream_insert(socket, :asic_mining_rig_list, mining_rig)
+        {:noreply, new_socket}
+    end
   end
 
   @impl true
