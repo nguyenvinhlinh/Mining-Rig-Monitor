e7ef449ae3be4733c901fe5f941c135c3e7cb3e5
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2025-01-03 22:54:30 +0700

    [feature-1] trigger mix phx.gen.auth and add live_view without app layout.

--- lib/mining_rig_monitor_web.ex
+++ lib/mining_rig_monitor_web.ex
@@ -58,6 +58,13 @@ defmodule MiningRigMonitorWeb do
     end
   end
 
+  def live_view_without_app_layout do
+    quote do
+      use Phoenix.LiveView
+      unquote(html_helpers())
+    end
+  end
+
   def live_component do
     quote do
       use Phoenix.LiveComponent
--- lib/mining_rig_monitor_web/router.ex
+++ lib/mining_rig_monitor_web/router.ex
@@ -1,6 +1,8 @@
 defmodule MiningRigMonitorWeb.Router do
   use MiningRigMonitorWeb, :router
 
+  import MiningRigMonitorWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule MiningRigMonitorWeb.Router do
     plug :put_root_layout, html: {MiningRigMonitorWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -63,6 +66,44 @@ defmodule MiningRigMonitorWeb.Router do
 
       live_dashboard "/dashboard", metrics: MiningRigMonitorWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", MiningRigMonitorWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{MiningRigMonitorWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", MiningRigMonitorWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{MiningRigMonitorWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", MiningRigMonitorWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{MiningRigMonitorWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
