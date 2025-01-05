9ddc0477dfc2d72d14e73779133017643007c23c
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2025-01-05 23:44:25 +0700

    remove live path that is ununsed.
    - /users/reset_password
    - /users/confirm

--- lib/mining_rig_monitor_web/router.ex
+++ lib/mining_rig_monitor_web/router.ex
@@ -77,11 +77,8 @@ defmodule MiningRigMonitorWeb.Router do
     live_session :redirect_if_user_is_authenticated,
       on_mount: [{MiningRigMonitorWeb.UserAuth, :redirect_if_user_is_authenticated}],
       root_layout: {MiningRigMonitorWeb.Layouts, :root_no_nav} do
-
       live "/users/register", UserRegistrationLive, :new
       live "/users/log_in", UserLoginLive, :new
-      live "/users/reset_password", UserForgotPasswordLive, :new
-      live "/users/reset_password/:token", UserResetPasswordLive, :edit
     end
 
     post "/users/log_in", UserSessionController, :create
@@ -93,7 +90,6 @@ defmodule MiningRigMonitorWeb.Router do
     live_session :require_authenticated_user,
       on_mount: [{MiningRigMonitorWeb.UserAuth, :ensure_authenticated}] do
       live "/users/settings", UserSettingsLive, :edit
-      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
     end
   end
 
@@ -101,11 +97,5 @@ defmodule MiningRigMonitorWeb.Router do
     pipe_through [:browser]
 
     delete "/users/log_out", UserSessionController, :delete
-
-    live_session :current_user,
-      on_mount: [{MiningRigMonitorWeb.UserAuth, :mount_current_user}] do
-      live "/users/confirm/:token", UserConfirmationLive, :edit
-      live "/users/confirm", UserConfirmationInstructionsLive, :new
-    end
   end
 end
