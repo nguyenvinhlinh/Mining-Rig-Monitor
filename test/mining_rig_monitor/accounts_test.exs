9e8401bd71ae84a93d33339ef761a1ab0aca9d7a
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2025-01-07 13:18:05 +0700

    update account test due to reduce password min length.

--- test/mining_rig_monitor/accounts_test.exs
+++ test/mining_rig_monitor/accounts_test.exs
@@ -59,11 +59,11 @@ defmodule MiningRigMonitor.AccountsTest do
     end
 
     test "validates email and password when given" do
-      {:error, changeset} = Accounts.register_user(%{email: "not valid", password: "not valid"})
+      {:error, changeset} = Accounts.register_user(%{email: "not valid", password: "123asd"})
 
       assert %{
                email: ["must have the @ sign and no spaces"],
-               password: ["should be at least 12 character(s)"]
+               password: ["should be at least 8 character(s)"]
              } = errors_on(changeset)
     end
 
@@ -171,12 +171,12 @@ defmodule MiningRigMonitor.AccountsTest do
     test "validates password", %{user: user} do
       {:error, changeset} =
         Accounts.update_user_password(user, valid_user_password(), %{
-          password: "not valid",
+          password: "123asd",
           password_confirmation: "another"
         })
 
       assert %{
-               password: ["should be at least 12 character(s)"],
+               password: ["should be at least 8 character(s)"],
                password_confirmation: ["does not match password"]
              } = errors_on(changeset)
     end
@@ -380,12 +380,12 @@ defmodule MiningRigMonitor.AccountsTest do
     test "validates password", %{user: user} do
       {:error, changeset} =
         Accounts.reset_user_password(user, %{
-          password: "not valid",
+          password: "123asd",
           password_confirmation: "another"
         })
 
       assert %{
-               password: ["should be at least 12 character(s)"],
+               password: ["should be at least 8 character(s)"],
                password_confirmation: ["does not match password"]
              } = errors_on(changeset)
     end
