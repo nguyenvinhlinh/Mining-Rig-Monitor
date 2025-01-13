9b2eb845c75dfdf1d227f93e65285ffe17d589c1
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2025-01-13 18:04:35 +0700

    update test in asic_miner_logs_test and asic_miner_log_controller_test.

--- test/mining_rig_monitor/asic_miner_logs_test.exs
+++ test/mining_rig_monitor/asic_miner_logs_test.exs
@@ -1,28 +1,21 @@
 defmodule MiningRigMonitor.AsicMinerLogsTest do
   use MiningRigMonitor.DataCase
 
+  import MiningRigMonitor.AsicMinersFixtures
+
   alias MiningRigMonitor.AsicMinerLogs
 
   describe "asic_miner_logs" do
     alias MiningRigMonitor.AsicMinerLogs.AsicMinerLog
-
     import MiningRigMonitor.AsicMinerLogsFixtures
 
-    @invalid_attrs %{}
-
-    test "list_asic_miner_logs/0 returns all asic_miner_logs" do
-      asic_miner_log = asic_miner_log_fixture()
-      assert AsicMinerLogs.list_asic_miner_logs() == [asic_miner_log]
+    @invalid_attrs %{"asic_miner_id" => nil}
+    setup do
+      %{asic_miner: asic_miner_fixture_by_commander()}
     end
 
-    test "get_asic_miner_log!/1 returns the asic_miner_log with given id" do
-      asic_miner_log = asic_miner_log_fixture()
-      assert AsicMinerLogs.get_asic_miner_log!(asic_miner_log.id) == asic_miner_log
-    end
-
-    test "create_asic_miner_log/1 with valid data creates a asic_miner_log" do
-      valid_attrs = %{}
-
+    test "create_asic_miner_log/1 with valid data creates a asic_miner_log", %{asic_miner: asic_miner} do
+      valid_attrs = %{"asic_miner_id" => asic_miner.id}
       assert {:ok, %AsicMinerLog{} = asic_miner_log} = AsicMinerLogs.create_asic_miner_log(valid_attrs)
     end
 
@@ -30,27 +23,8 @@ defmodule MiningRigMonitor.AsicMinerLogsTest do
       assert {:error, %Ecto.Changeset{}} = AsicMinerLogs.create_asic_miner_log(@invalid_attrs)
     end
 
-    test "update_asic_miner_log/2 with valid data updates the asic_miner_log" do
-      asic_miner_log = asic_miner_log_fixture()
-      update_attrs = %{}
-
-      assert {:ok, %AsicMinerLog{} = asic_miner_log} = AsicMinerLogs.update_asic_miner_log(asic_miner_log, update_attrs)
-    end
-
-    test "update_asic_miner_log/2 with invalid data returns error changeset" do
-      asic_miner_log = asic_miner_log_fixture()
-      assert {:error, %Ecto.Changeset{}} = AsicMinerLogs.update_asic_miner_log(asic_miner_log, @invalid_attrs)
-      assert asic_miner_log == AsicMinerLogs.get_asic_miner_log!(asic_miner_log.id)
-    end
-
-    test "delete_asic_miner_log/1 deletes the asic_miner_log" do
-      asic_miner_log = asic_miner_log_fixture()
-      assert {:ok, %AsicMinerLog{}} = AsicMinerLogs.delete_asic_miner_log(asic_miner_log)
-      assert_raise Ecto.NoResultsError, fn -> AsicMinerLogs.get_asic_miner_log!(asic_miner_log.id) end
-    end
-
-    test "change_asic_miner_log/1 returns a asic_miner_log changeset" do
-      asic_miner_log = asic_miner_log_fixture()
+    test "change_asic_miner_log/1 returns a asic_miner_log changeset", %{asic_miner: asic_miner} do
+      asic_miner_log = asic_miner_log_fixture(%{asic_miner_id: asic_miner.id})
       assert %Ecto.Changeset{} = AsicMinerLogs.change_asic_miner_log(asic_miner_log)
     end
   end
--- test/mining_rig_monitor_web/controllers/asic_miner_log_controller_test.exs
+++ test/mining_rig_monitor_web/controllers/asic_miner_log_controller_test.exs
@@ -17,63 +17,6 @@ defmodule MiningRigMonitorWeb.AsicMinerLogControllerTest do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
 
-  describe "index" do
-    test "lists all asic_miner_logs", %{conn: conn} do
-      conn = get(conn, ~p"/api/asic_miner_logs")
-      assert json_response(conn, 200)["data"] == []
-    end
-  end
-
-  describe "create asic_miner_log" do
-    test "renders asic_miner_log when data is valid", %{conn: conn} do
-      conn = post(conn, ~p"/api/asic_miner_logs", asic_miner_log: @create_attrs)
-      assert %{"id" => id} = json_response(conn, 201)["data"]
-
-      conn = get(conn, ~p"/api/asic_miner_logs/#{id}")
-
-      assert %{
-               "id" => ^id
-             } = json_response(conn, 200)["data"]
-    end
-
-    test "renders errors when data is invalid", %{conn: conn} do
-      conn = post(conn, ~p"/api/asic_miner_logs", asic_miner_log: @invalid_attrs)
-      assert json_response(conn, 422)["errors"] != %{}
-    end
-  end
-
-  describe "update asic_miner_log" do
-    setup [:create_asic_miner_log]
-
-    test "renders asic_miner_log when data is valid", %{conn: conn, asic_miner_log: %AsicMinerLog{id: id} = asic_miner_log} do
-      conn = put(conn, ~p"/api/asic_miner_logs/#{asic_miner_log}", asic_miner_log: @update_attrs)
-      assert %{"id" => ^id} = json_response(conn, 200)["data"]
-
-      conn = get(conn, ~p"/api/asic_miner_logs/#{id}")
-
-      assert %{
-               "id" => ^id
-             } = json_response(conn, 200)["data"]
-    end
-
-    test "renders errors when data is invalid", %{conn: conn, asic_miner_log: asic_miner_log} do
-      conn = put(conn, ~p"/api/asic_miner_logs/#{asic_miner_log}", asic_miner_log: @invalid_attrs)
-      assert json_response(conn, 422)["errors"] != %{}
-    end
-  end
-
-  describe "delete asic_miner_log" do
-    setup [:create_asic_miner_log]
-
-    test "deletes chosen asic_miner_log", %{conn: conn, asic_miner_log: asic_miner_log} do
-      conn = delete(conn, ~p"/api/asic_miner_logs/#{asic_miner_log}")
-      assert response(conn, 204)
-
-      assert_error_sent 404, fn ->
-        get(conn, ~p"/api/asic_miner_logs/#{asic_miner_log}")
-      end
-    end
-  end
 
   defp create_asic_miner_log(_) do
     asic_miner_log = asic_miner_log_fixture()
