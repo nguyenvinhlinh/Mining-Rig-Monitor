a3dffe903b5f1f73c74d6665c398d5ed80fefc9f
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2025-01-15 13:34:16 +0700

    after login, redirect user to /asic_miners.

--- lib/mining_rig_monitor_web/user_auth.ex
+++ lib/mining_rig_monitor_web/user_auth.ex
@@ -225,5 +225,5 @@ defmodule MiningRigMonitorWeb.UserAuth do
 
   defp maybe_store_return_to(conn), do: conn
 
-  defp signed_in_path(_conn), do: ~p"/"
+  defp signed_in_path(_conn), do: ~p"/asic_miners"
 end
