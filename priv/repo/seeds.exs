a2b1f3080f0a299455d59bafc87cba5f45fbd975
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2025-03-17 17:07:04 +0700

    [feature-16] add seed for address. also update other seed.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -14,38 +14,138 @@
 
 alias MiningRigMonitor.Repo
 alias MiningRigMonitor.AsicMiners.AsicMiner
+alias MiningRigMonitor.CpuGpuMiners.CpuGpuMiner
+alias MiningRigMonitor.Addresses.Address
 
-asic_1 = %AsicMiner{
-  name: "ASIC_1", api_code: "asic_1_code",
+Repo.delete_all(MiningRigMonitor.AsicMinerLogs.AsicMinerLog)
+Repo.delete_all(MiningRigMonitor.CpuGpuMinerLogs.CpuGpuMinerLog)
+Repo.delete_all(AsicMiner)
+Repo.delete_all(CpuGpuMiner)
+Repo.delete_all(Address)
+
+%AsicMiner{
+  name: "ASIC_1", api_code: "api_code_1",
   model: "Ice River KS5M",
   model_variant: "10TH",
   firmware_version: "BOOT_3_1_image_1.0",
-  software_version: "ICM168_3_2_10_ks5L_miner_ICM168_3_2_10_ks5L_bg"
+  software_version: "ICM168_3_2_10_ks5L_miner_ICM168_3_2_10_ks5L_bg",
+  activated: true
 }
 |> Repo.insert!
 
-asic_2 = %AsicMiner{
-  name: "ASIC_2", api_code: "asic_2_code",
+%AsicMiner{
+  name: "ASIC_2", api_code: "api_code_2",
   model: "Ice River KS5M",
   model_variant: "12TH",
   firmware_version: "BOOT_3_1_image_1.0",
-  software_version: "ICM168_3_2_10_ks5L_miner_ICM168_3_2_10_ks5L_bg"
+  software_version: "ICM168_3_2_10_ks5L_miner_ICM168_3_2_10_ks5L_bg",
+  activated: true
 }
 |> Repo.insert!
 
-asic_3 = %AsicMiner{
-  name: "ASIC_3", api_code: "asic_3_code",
+%AsicMiner{
+  name: "ASIC_3", api_code: "api_code_3",
   model: "Ice River KS5M",
   model_variant: "15TH",
   firmware_version: "BOOT_3_1_image_1.0",
-  software_version: "ICM168_3_2_10_ks5L_miner_ICM168_3_2_10_ks5L_bg"
+  software_version: "ICM168_3_2_10_ks5L_miner_ICM168_3_2_10_ks5L_bg",
+  activated: false
+}
+|> Repo.insert!
+
+%CpuGpuMiner{
+  name: "CPU/GPU Miner 1",
+  api_code: "api_code_1",
+  motherboard_name: "B850 MSI",
+  cpu_name: "Ryzen 7950x3D",
+  ram_size: "8GB",
+  gpu_1_name: "RTX 3080 MSI",
+  gpu_2_name: "RTX 3080 MSI",
+  gpu_3_name: "RTX 3080 MSI",
+  gpu_4_name: "RTX 3080 MSI",
+  gpu_5_name: "RTX 3080 MSI",
+  gpu_6_name: "RTX 3080 MSI",
+  gpu_7_name: "RTX 3080 MSI",
+  gpu_8_name: "RTX 3080 MSI",
+  activated: true
+}
+|> Repo.insert!
+
+%CpuGpuMiner{
+  name: "CPU/GPU Miner 2",
+  api_code: "api_code_2",
+  motherboard_name: "B850 MSI",
+  cpu_name: "Ryzen 7950x3D",
+  ram_size: "16GB",
+  gpu_1_name: "RTX 3080 MSI",
+  gpu_2_name: "RTX 3080 MSI",
+  gpu_3_name: "RTX 3080 MSI",
+  gpu_4_name: "RTX 3080 MSI",
+  gpu_5_name: "RTX 3080 MSI",
+  gpu_6_name: "RTX 3080 MSI",
+  gpu_7_name: "RTX 3080 MSI",
+  gpu_8_name: "RTX 3080 MSI",
+  activated: true
+}
+|> Repo.insert!
+
+%CpuGpuMiner{
+  name: "CPU/GPU Miner 3",
+  api_code: "api_code_3",
+  motherboard_name: "B850 MSI",
+  cpu_name: "Ryzen 7950x3D",
+  ram_size: "256GB",
+  gpu_1_name: "RTX 3080 MSI",
+  gpu_2_name: "RTX 3080 MSI",
+  gpu_3_name: "RTX 3080 MSI",
+  gpu_4_name: "RTX 3080 MSI",
+  gpu_5_name: "RTX 3080 MSI",
+  gpu_6_name: "RTX 3080 MSI",
+  gpu_7_name: "RTX 3080 MSI",
+  gpu_8_name: "RTX 3080 MSI",
+  activated: false
+}
+|> Repo.insert!
+
+%Address{
+  name: "Kaspa",
+  type: "wallet",
+  address: "kaspa:qp9rv9jvx2kyf6wu4lupuruunq5zsuszyxs0dr3l89ej7wsgs48jqkewy6xtl"
+}
+|> Repo.insert!
+
+%Address{
+  name: "Monero",
+  type: "wallet",
+  address: "4AUzXvUpuuRCh3PqMhBmUyfP3TFPhz5cL1iDbgThY6rnUv9vtbPACW2TMJZ8ArprwheeBXEc9rYh62m6vZ8nLW7a8t9iBDn"
+}
+|> Repo.insert!
+
+%Address{
+  name: "Ethereum",
+  type: "wallet",
+  address: "0x999999cf1046e68e36E1aA2E0E07105eDDD1f08E"
 }
 |> Repo.insert!
 
 
+%Address{
+  name: "Woolypooly-kaspa",
+  type: "pool",
+  address: "pool.br.woolypooly.com:3112"
+}
+|> Repo.insert!
 
+%Address{
+  name: "Hashvault-monero",
+  type: "pool",
+  address: "pool.hashvault.pro:443"
+}
+|> Repo.insert!
 
-
-#cpu_gpu_1 = %MiningRig{name: "cpu_gpu_1", code: "cpu_gpu_1_code"} |> Repo.insert!
-#cpu_gpu_2 = %MiningRig{name: "cpu_gpu_2", code: "cpu_gpu_2_code"} |> Repo.insert!
-#cpu_gpu_3 = %MiningRig{name: "cpu_gpu_3", code: "cpu_gpu_3_code"} |> Repo.insert!
+%Address{
+  name: "Herominer-beam",
+  type: "pool",
+  address: "sg.beam.gfwroute.com:1130"
+}
+|> Repo.insert!
