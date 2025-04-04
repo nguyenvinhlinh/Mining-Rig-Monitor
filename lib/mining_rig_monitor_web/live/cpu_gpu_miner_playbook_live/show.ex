1cea03cf87e2322bc67f3854ea505f2e871b96c1
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2025-04-04 22:08:32 +0700

    add show page for playbook.

--- lib/mining_rig_monitor_web/live/cpu_gpu_miner_playbook_live/show.ex
+++ lib/mining_rig_monitor_web/live/cpu_gpu_miner_playbook_live/show.ex
@@ -1,8 +1,10 @@
 defmodule MiningRigMonitorWeb.CpuGpuMinerPlaybookLive.Show do
   use MiningRigMonitorWeb, :live_view
+  require Logger
 
   alias MiningRigMonitor.CpuGpuMinerPlaybooks
-  alias  MiningRigMonitor.CpuGpuMiners
+  alias MiningRigMonitor.CpuGpuMiners
+  alias MiningRigMonitor.Repo
 
   embed_templates "show_html/*"
 
@@ -14,6 +16,9 @@ defmodule MiningRigMonitorWeb.CpuGpuMinerPlaybookLive.Show do
   @impl true
   def handle_params(%{"playbook_id" => id}, _, socket) do
     cpu_gpu_miner_playbook = CpuGpuMinerPlaybooks.get_cpu_gpu_miner_playbook!(id)
+    |> Repo.preload([:cpu_wallet_address, :gpu_wallet_address_1, :gpu_wallet_address_2,
+                   :cpu_pool_address, :gpu_pool_address_1, :gpu_pool_address_2])
+    Logger.warning("[CpuGpuMinerPlaybookLive.Show] Preload cpu_gpu_miner_playbook_list's addresses will be overload. Should be improved!")
     cpu_gpu_miner = CpuGpuMiners.get_cpu_gpu_miner!(cpu_gpu_miner_playbook.cpu_gpu_miner_id)
     socket_mod = socket
     |> assign(:page_title, page_title(socket.assigns.live_action))
