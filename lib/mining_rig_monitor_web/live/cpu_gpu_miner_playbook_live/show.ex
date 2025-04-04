21af886a81b7e72376279a140fc67773eda54cec
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2025-04-04 20:45:55 +0700

    add cpu_gpu_miner_playbook_live/show_html/_show_top.html.heex

--- lib/mining_rig_monitor_web/live/cpu_gpu_miner_playbook_live/show.ex
+++ lib/mining_rig_monitor_web/live/cpu_gpu_miner_playbook_live/show.ex
@@ -2,6 +2,9 @@ defmodule MiningRigMonitorWeb.CpuGpuMinerPlaybookLive.Show do
   use MiningRigMonitorWeb, :live_view
 
   alias MiningRigMonitor.CpuGpuMinerPlaybooks
+  alias  MiningRigMonitor.CpuGpuMiners
+
+  embed_templates "show_html/*"
 
   @impl true
   def mount(_params, _session, socket) do
@@ -10,11 +13,15 @@ defmodule MiningRigMonitorWeb.CpuGpuMinerPlaybookLive.Show do
 
   @impl true
   def handle_params(%{"playbook_id" => id}, _, socket) do
-    {:noreply,
-     socket
-     |> assign(:page_title, page_title(socket.assigns.live_action))
-     |> assign(:cpu_gpu_miner_playbook, CpuGpuMinerPlaybooks.get_cpu_gpu_miner_playbook!(id))}
-  end
+    cpu_gpu_miner_playbook = CpuGpuMinerPlaybooks.get_cpu_gpu_miner_playbook!(id)
+    cpu_gpu_miner = CpuGpuMiners.get_cpu_gpu_miner!(cpu_gpu_miner_playbook.cpu_gpu_miner_id)
+    socket_mod = socket
+    |> assign(:page_title, page_title(socket.assigns.live_action))
+    |> assign(:cpu_gpu_miner, cpu_gpu_miner)
+    |> assign(:cpu_gpu_miner_playbook, cpu_gpu_miner_playbook)
+    {:noreply, socket_mod}
+
+    end
 
   defp page_title(:show), do: "Show CPU/GPU miner playbook"
 end
