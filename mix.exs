6ed73e9180293295fac3f9e7cef7ebfad94238d6
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2025-04-12 22:40:59 +0700

    change version from 2.0.0 to 3.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MiningRigMonitor.MixProject do
   def project do
     [
       app: :mining_rig_monitor,
-      version: "2.0.0",
+      version: "3.0.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
